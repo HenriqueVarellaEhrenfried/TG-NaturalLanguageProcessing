31b263b20db8215d03ecf3713f1cd620a7b2c53d
Henrique Varella Ehrenfried <hve13@inf.ufpr.br>
2016-05-21 15:00:24 +0000

    Make a few improvements

--- tg2.py
+++ tg2.py
@@ -9,14 +9,14 @@ import re
 # sentence = 'Michael Wasalsky and Phil Sulivan could have frightened those children'
 # sentence = 'The book was read by me'
 # sentence = 'Because the boys went to the park, they did not go to the zoo'
-sentence = 'A Company needs a new system called, Payroll System, to allow employees to record timecard information electronically and automatically generate paychecks based on the number of hours worked and total amount of sales(for commissioned employees)'
+# sentence = 'A Company needs a new system called, Payroll System, to allow employees to record timecard information electronically and automatically generate paychecks based on the number of hours worked and total amount of sales(for commissioned employees)'
+# sentence = "A Company needs a new system called, Payroll System, to allow employees to record timecard information electronically and automatically generate paychecks based on the number of hours worked and total amount of sales(for commissioned employees). The new system will be state of art and will have a Windows-based desktop interface to allow employees to enter timecard information, enter purchase orders, change employee preferences (such as payment method) and create various reports. The system will run on individual employee desktops throughout the entire company.The system will retain information on all employees in the company. The system must pay each employee the correct amount, on time, by the method that they specify.Some employees work by the hour and are paid an hourly rate. They submit timecards that record the date and number of hours worked for a particular charge number. Some employees are paid a flat salary. Even though they are paid a flat salary, they submit timecards that record the date and hours worked. Some of the salaried employees also receive a commission based on their sales. They submit purchase orders that reflect the date and amount of the sale.One of the most requested features of the new system is employee reporting.Employees will be able to query the system for hours worked, totals of all hours billed to a project, total pay received year to date, etc.,Employees can choose their method of payment. They can have their paychecks mailed to the postal address of their choice, or they can request direct deposit and have their paycheck deposited into a bank account of their choosing. The employee may also choose to pick their paychecks up at the offices. The Payroll Administrator maintains employee information. He is responsible for adding new employees, deleting employees and changing all employee information such as name, address and payment classification(hourly, salaried, commissioned), as well as running administrative reports.The Payroll application will run automatically every Friday and on the last working day of month. It will pay the appropriate employees on those days. The system will be told what date employees are to be paid, so it will generate payments for records from the last time the employee was paid to the specified date. The new system is being designed so that the payroll will always be generated automatically and there will be need for any manual intervention"
 problem = "A Company needs a new system called, Payroll System, to allow employees to record timecard information electronically and automatically generate paychecks based on the number of hours worked and total amount of sales(for commissioned employees). The new system will be state of art and will have a Windows-based desktop interface to allow employees to enter timecard information, enter purchase orders, change employee preferences (such as payment method) and create various reports. The system will run on individual employee desktops throughout the entire company.The system will retain information on all employees in the company. The system must pay each employee the correct amount, on time, by the method that they specify.Some employees work by the hour and are paid an hourly rate. They submit timecards that record the date and number of hours worked for a particular charge number. Some employees are paid a flat salary. Even though they are paid a flat salary, they submit timecards that record the date and hours worked. Some of the salaried employees also receive a commission based on their sales. They submit purchase orders that reflect the date and amount of the sale.One of the most requested features of the new system is employee reporting.Employees will be able to query the system for hours worked, totals of all hours billed to a project, total pay received year to date, etc.,Employees can choose their method of payment. They can have their paychecks mailed to the postal address of their choice, or they can request direct deposit and have their paycheck deposited into a bank account of their choosing. The employee may also choose to pick their paychecks up at the offices. The Payroll Administrator maintains employee information. He is responsible for adding new employees, deleting employees and changing all employee information such as name, address and payment classification(hourly, salaried, commissioned), as well as running administrative reports.The Payroll application will run automatically every Friday and on the last working day of month. It will pay the appropriate employees on those days. The system will be told what date employees are to be paid, so it will generate payments for records from the last time the employee was paid to the specified date. The new system is being designed so that the payroll will always be generated automatically and there will be need for any manual intervention"
 sentences = problem.split(".")
 
 nlp = English()
 # doc = nlp(unicode(sentence))
 verb = WordNetLemmatizer()
-
 
 def get_subject(doc):
     subject_init_token = []
@@ -31,13 +31,11 @@ def get_subject(doc):
         subjects.append(partial_subtree_subject)
     return (subjects, subject_init_token)
 
-
 def get_infinitive_verb(vb):
     verbs = []
     for v in vb:
         verbs.append(verb.lemmatize(v.text.encode('ascii', 'replace'), 'v'))
     return verbs
-
 
 def get_verb(subj):
     verbs = []
@@ -55,76 +53,19 @@ def get_comp(verbs):
                 partial_subtree_object.append(t1)
         objects.append([partial_subtree_object,[t.head,t]])
     return (objects)
-    
-    
-    
-    
-def get_complement(verbs):
-    objects = []
-    for token in verbs:
-        for t in token.rights:
-            partial_subtree_object = []
-            for t1 in t.subtree:
-                partial_subtree_object.append(t1)
-            objects.append([partial_subtree_object,[t.head,t]])
-    return (objects)
-def  get_number_of_description(description, action):
-    number = len(description)
-    i = 0
-    soma = 0
-    while i < number:
-        if description[i][1][0] == action:
-            soma = soma + 1
-        i = i + 1
-    return soma
-    
-def generate_possible_user_cases(subj,verbs,obj):
-    print "----------SUJEITO----------"
-    print subj
-    print "----------VERBO----------"
-    print verbs
-    print "----------OBJETO----------"
-    print obj
-    index = 0
-    number_of_verbs = len(verbs)
-    print "----------NUM_VERBS----------"
-    print number_of_verbs
-    description = obj
-    description_iterator = 0
-    number_of_description = 0
-    print("Ator\tAcao\tComplemento")
-    while index < number_of_verbs:
-        actor = subj[0][index]
-        action = verbs[index]
-        number_of_description = number_of_description + get_number_of_description(description, action)
-        while description_iterator < number_of_description:
-            possible_obj = description[description_iterator]
-            if  possible_obj[1][0] == action:
-                if description[description_iterator][0] != None:
-                    complement = description[description_iterator][0]
-                else:
-                    complement = None
-                if complement:
-                    print(str(actor)+"\t"+str(action)+"\t"+str(complement))
-                else:
-                    print(str(actor)+"\t"+str(action)+"\t"+"---Nao ha complemento---")
-            description_iterator = description_iterator + 1
-        index = index + 1
-
-
 
 allSentence = []
 
-# for sentence in sentences:
-print(sentence)
-doc = nlp(unicode(sentence))
-subj = get_subject(doc)
-verbs = get_verb(subj)
-comp = get_comp(verbs)
+for sentence in sentences:
+    doc = nlp(unicode(sentence))
+    subj = get_subject(doc)
+    verbs = get_verb(subj)
+    comp = get_comp(verbs)
+    dict = {'Sujeito': subj, 'Verbos': verbs, 'Complemento': comp, 'Sentence': sentence, 'Num_Suj': len(subj[1])}
+    allSentence.append(dict)
+    print(dict)
 
-print(comp)
-
-
+#TODO: Agregar(incluindo os pronomes) sujeitos e gerar os possiveis casos de teste. 
 
 
 
